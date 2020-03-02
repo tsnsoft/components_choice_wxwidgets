55339351e8a0c9dc9b1cbc76f7be51b7db554be0
proffix4 <proffix4@gmail.com>
2020-03-02 15:20:26 +0600

    new

---
+++ components_choice_wxwidgetsApp.cpp
@@ -0,0 +1,33 @@
+/***************************************************************
+ * Name:      components_choice_wxwidgetsApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    Talipov S.N. (talipovsn@gmail.com)
+ * Created:   2020-03-02
+ * Copyright: Talipov S.N. (https://github.com/tsnsoft)
+ * License:
+ **************************************************************/
+
+#include "components_choice_wxwidgetsApp.h"
+
+//(*AppHeaders
+#include "components_choice_wxwidgetsMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(components_choice_wxwidgetsApp);
+
+bool components_choice_wxwidgetsApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	components_choice_wxwidgetsFrame* Frame = new components_choice_wxwidgetsFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
